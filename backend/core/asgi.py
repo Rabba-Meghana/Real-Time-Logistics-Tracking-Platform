4c67f80d4b60827259ab0d9df5ed2da380570454
Meghana <meghana@opentug.com>
2026-04-14 03:48:33 +0000

    fix: remove AllowedHostsOriginValidator to fix WebSocket 403 on EC2

--- backend/core/asgi.py
+++ backend/core/asgi.py
@@ -2,7 +2,7 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from channels.security.websocket import AllowedHostsOriginValidator
+from channels.security.websocket import OriginValidator
 import vessels.routing
 import voyages.routing
 
@@ -10,12 +10,10 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),
-    'websocket': AllowedHostsOriginValidator(
-        AuthMiddlewareStack(
-            URLRouter(
-                vessels.routing.websocket_urlpatterns +
-                voyages.routing.websocket_urlpatterns
-            )
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            vessels.routing.websocket_urlpatterns +
+            voyages.routing.websocket_urlpatterns
         )
     ),
 })
